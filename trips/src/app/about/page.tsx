5cb854ae89f6196ab7a0edb0d372e4dfadfc2dd8
Pablwo Araujo <pablwo.araujo@ccc.ufcg.edu.br>
2023-07-05 16:10:05 -0300

    removendo os estilos default

--- trips/src/app/about/page.tsx
+++ trips/src/app/about/page.tsx
@@ -1,11 +1,14 @@
 import Trips from "./components/Trips";
 
+export const metadata = {
+  title: "About",
+};
+
 const About = async () => {
   return (
     <>
       <h1>About</h1>
       <Trips />
-      
     </>
   );
 };
